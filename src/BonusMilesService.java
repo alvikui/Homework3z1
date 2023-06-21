ba5a9ae9091d47c13f551633c4e07fdd25e18a49
Alena Kuimova <alvikui@yandex.ru>
2023-06-21 14:38:48 +0600

    Change src/

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,7 @@ public class BonusMilesService {
 
     public int calculate(int price) {
         int result;
-        if (price >= 20) {
+        if (price > 20) {
             result = price / 20;
         } else {
             result = 0;
