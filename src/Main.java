1b6b2ced1acc344e19559ede58cdafdf291b58b4
Alena Kuimova <alvikui@yandex.ru>
2023-06-21 12:22:27 +0600

    Change src

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticket = 13_676; //Стоимость авиабилета
-        int accrual = 20; //Начисление одной мили
-
-        int miles = ticket / accrual;
-
-        System.out.println("Количество начисленных миль: " + miles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 13_676;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
